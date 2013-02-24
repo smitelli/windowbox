ae25ac670854f0c87314b0cdc25cbc8cdf459974
Scott Smitelli <scott@smitelli.com>
2013-02-24 01:31:08 -0500

    Image cropping and resizing

--- models.py
+++ models.py
@@ -1,3 +1,6 @@
+import Image
+from StringIO import StringIO
+
 from sqlalchemy import Column, Integer, String, LargeBinary
 from database import Base
 
@@ -19,10 +22,18 @@ class Post(Base):
         self.ua = ua
 
     def __repr__(self):
-        return '<Post {}>'.format(self.id)
+        return '<Post id={}>'.format(self.id)
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.order_by(cls.id).all()
+
+    @classmethod
+    def get_by_id(cls, post_id):
+        return cls.query.filter(cls.id == post_id).first()
 
     def get_image(self):
-        return ImageData.query.filter(ImageData.image_id == self.image_id).first()
+        return ImageData.get_by_id(self.image_id)
 
 
 class ImageData(Base):
@@ -37,4 +48,45 @@ class ImageData(Base):
         self.data = data
 
     def __repr__(self):
-        return '<ImageData {}>'.format(self.image_id)
+        return '<ImageData image_id={}>'.format(self.image_id)
+
+    @classmethod
+    def get_by_id(cls, image_id):
+        return cls.query.filter(cls.image_id == image_id).first()
+
+    def get_resized_data(self, width=None, height=None):
+        width, height = int(width or 0), int(height or 0)
+
+        im = Image.open(StringIO(self.data))
+        old_width, old_height = im.size
+        crop = 0, 0, old_width, old_height
+
+        if width and height:
+            fx = float(old_width) / width
+            fy = float(old_height) / height
+            f = fx if fx < fy else fy
+
+            crop_width, crop_height = int(width * f), int(height * f)
+
+            trim_x = (old_width - crop_width) / 2
+            trim_y = (old_height - crop_height) / 2
+
+            crop = trim_x, trim_y, crop_width + trim_x, crop_height + trim_y
+            size = width, height
+
+        elif width and not height:
+            f = float(old_width) / width
+            size = width, int(old_height / f)
+
+        elif height and not width:
+            f = float(old_height) / height
+            size = int(old_width / f), height
+
+        else:
+            size = old_width, old_height
+
+        im = im.transform(size, Image.EXTENT, crop)
+
+        im_io = StringIO()
+        im.save(im_io, 'JPEG', quality=100)
+        return im_io.getvalue()
--- windowbox.py
+++ windowbox.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, abort
+from re import match
 from database import db_session
 from models import Post
 
@@ -7,30 +8,47 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    posts = Post.query.order_by(Post.id).all()
+    posts = Post.get_all()
 
     return render_template('index.html', posts=posts)
 
 
 @app.route('/post/<int:post_id>')
 def single_post(post_id):
-    post = Post.query.filter(Post.id == post_id).first()
+    post = Post.get_by_id(post_id)
+
+    if post is None:
+        abort(404)
 
     return render_template('single_post.html', post=post)
 
 
-@app.route('/image/<int:post_id>')
-def image_data(post_id):
-    image = Post.query.filter(Post.id == post_id).first().get_image()
+@app.route('/image/<int:post_id>', defaults={'size': ''})
+@app.route('/image/<int:post_id>/<size>')
+def image_data(post_id, size):
+    post = Post.get_by_id(post_id)
 
-    response = make_response(image.data)
+    if post is None:
+        abort(404)
+
+    image = post.get_image()
+
+    matches = match('(\d*)x(\d*)', size)
+    w, h = matches.groups() if matches else (None, None)
+
+    response = make_response(image.get_resized_data(width=w, height=h))
     response.headers['Content-Type'] = image.mime_type
     return response
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'You screwed up!', 404
 
 
 @app.teardown_request
 def shutdown_session(exception=None):
     db_session.remove()
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
