35bfd38900304711b78476945a64fc833a4c0845
Scott Smitelli <scott@smitelli.com>
2014-02-16 19:12:24 -0500

    Lowercase 'utc' seems to be preferred by pytz docs

--- windowbox/database.py
+++ windowbox/database.py
@@ -17,10 +17,10 @@ class UTCDateTime(sa.types.TypeDecorator):
 
     def process_bind_param(self, value, dialect):
         if value is not None:
-            return value.astimezone(pytz.UTC)
+            return value.astimezone(pytz.utc)
 
     def process_result_value(self, value, dialect):
         if value is not None:
             return datetime(
                 value.year, value.month, value.day, value.hour, value.minute,
-                value.second, value.microsecond, tzinfo=pytz.UTC)
+                value.second, value.microsecond, tzinfo=pytz.utc)
