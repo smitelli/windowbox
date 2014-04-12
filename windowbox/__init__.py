5fec669f194ffb6b30c9576ea9dcdaeb9bbb5dc2
Scott Smitelli <scott@smitelli.com>
2014-04-12 16:20:35 -0400

    Add AppPath class

---
+++ windowbox/__init__.py
@@ -0,0 +1,28 @@
+from os import path
+
+# This is the absolute, real path to the 'windowbox' directory
+_src = path.abspath(path.dirname(__file__))
+
+
+class AppPath(object):
+    """Collection of class methods to build paths relative to any directory."""
+
+    @classmethod
+    def windowbox(cls, subpath):
+        return path.join(_src, subpath)
+
+    @classmethod
+    def configs(cls, subpath):
+        return path.join(_src, 'configs', subpath)
+
+    @classmethod
+    def handlers(cls, subpath):
+        return path.join(_src, 'handlers', subpath)
+
+    @classmethod
+    def models(cls, subpath):
+        return path.join(_src, 'models', subpath)
+
+    @classmethod
+    def templates(cls, subpath):
+        return path.join(_src, 'templates', subpath)
