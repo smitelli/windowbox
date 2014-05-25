57c02fc653c5b1f36a7af1dcbc196d501badbd99
Scott Smitelli <scott@smitelli.com>
2014-05-25 14:52:22 -0400

    Move inline scripts the hell out

---
+++ windowbox/static/js/handlers/single_post.js
@@ -0,0 +1,19 @@
+var $ = require('jquery');
+
+module.exports = function () {
+    var inPos  = $('.arrow.previous').css('left'),
+        outPos = -$('.arrow.previous').width();
+
+    $('.arrow.previous').css('left', outPos);
+    $('.arrow.next').css('right', outPos);
+
+    $('#attachment').hover(function () {
+        $('.arrow').stop();
+        $('.arrow.previous').animate({ left : inPos });
+        $('.arrow.next').animate({ right : inPos });
+    }, function () {
+        $('.arrow').stop();
+        $('.arrow.previous').animate({ left : outPos });
+        $('.arrow.next').animate({ right : outPos });
+    });
+};
--- windowbox/static/js/main.js
+++ windowbox/static/js/main.js
@@ -1,1 +1,14 @@
-global.$ = require('jquery');
+var $ = require('jquery'),
+    _ = require('lodash'),
+
+    handlers = {
+        '#single-post' : require('./handlers/single_post.js')
+    };
+
+$(function () {
+    _(handlers).omit(function (handler, selector) {
+        return $(selector).length === 0;
+    }).forOwn(function (handler) {
+        handler();
+    });
+});
