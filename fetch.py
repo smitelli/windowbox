bf937f4fc6917a384413c597b616cb90cfa6abed
Scott Smitelli <scott@smitelli.com>
2014-06-08 16:14:20 -0400

    Holy nuts, it works.

--- fetch.py
+++ fetch.py
@@ -1,14 +1,34 @@
+from windowbox.models.attachment import Attachment
 from windowbox.models.imap import IMAPManager
+from windowbox.models.post import Post
 
 
-mgr = IMAPManager('imap.gmail.com', '', '')
-for i in mgr.scrape_mailbox('Inbox', delete=False):
+imap_manager = IMAPManager('imap.gmail.com', '', '')
+messages = imap_manager.scrape_mailbox('Inbox')
 
-    print i.message_body
-    print i.created_utc
-    print i.sender
-    print i.user_agent
+for message in messages:
+    real_name, email = message.sender
+    if email != 'scott@smitelli.com':
+        print 'Skipping, {} is not a permitted sender'.format(email)
+        continue
 
-    at = i.get_attachment_data('image/jpeg')
-    if at:
-        print len(at)
+    valid_types = Attachment.mime_extension_map.keys()
+    attach_data = message.get_attachment_data(valid_types)
+
+    if not attach_data:
+        print 'Skipping, no usable attachment'
+        continue
+
+    post_data = {
+        'created_utc': message.created_utc,
+        'message': message.message_body,
+        'user_agent': message.user_agent}
+    post = Post(**post_data).save(commit=True)
+
+    attachment = Attachment(post_id=post.id)
+    attachment.set_data(attach_data)
+    attachment.save(commit=True)
+
+    print 'ok'
+
+imap_manager.close()
