762d59b12500f333f9323d167d6460bc9bb9170a
Scott Smitelli <scott@smitelli.com>
2014-06-24 10:53:57 -0400

    Log get_or_404's thoughts

--- windowbox/handlers/__init__.py
+++ windowbox/handlers/__init__.py
@@ -1,6 +1,6 @@
 import collections
 from datetime import datetime
-from flask import abort, g, request
+from flask import abort, current_app, g, request
 
 
 class AppGlobals(object):
@@ -31,9 +31,11 @@ def get_or_404(method, *args, **kwargs):
     result = method(*args, **kwargs)
 
     if result is None:
+        current_app.logger.debug('Bailing with 404; result is None')
         abort(404)
 
     if isinstance(result, collections.Iterable) and not any(result):
+        current_app.logger.debug('Bailing with 404; result Iterable has no truthy items')
         abort(404)
 
     return result
