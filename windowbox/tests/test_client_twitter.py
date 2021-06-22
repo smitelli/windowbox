90da35bb933ddbbaf8038e52a0f4fa30d0de38e9
Scott Smitelli <scott@smitelli.com>
2021-06-21 20:23:06 -0400

    Fix up Twitter client tests

--- windowbox/tests/test_client_twitter.py
+++ windowbox/tests/test_client_twitter.py
@@ -18,6 +18,7 @@ def twitter():
         access_token='accesstoken4twitter',
         access_token_secret='accesstokensecret4twitter',
         status_length=140,  # we're old school like that
+        url_length=30,
         timeout=15)
 
 
@@ -30,6 +31,7 @@ def test_constructor(twitter):
     assert twitter.access_token == 'accesstoken4twitter'
     assert twitter.access_token_secret == 'accesstokensecret4twitter'
     assert twitter.status_length == 140
+    assert twitter.url_length == 30
     assert twitter.timeout == 15
     assert twitter.client is None
 
@@ -45,27 +47,6 @@ def test_make_client(twitter):
     assert client.auth.access_token == 'accesstoken4twitter'
     assert client.auth.access_token_secret == 'accesstokensecret4twitter'
     assert client.timeout == 15
-
-
-def test_url_length(twitter):
-    """
-    Should read URL lengths from the remote configuration.
-
-    If HTTPS and non-HTTPS lengths differ, the larger is used.
-    """
-    mock_client = Mock()
-    twitter._make_client = Mock(return_value=mock_client)
-
-    mock_client.configuration.return_value = {
-        'short_url_length': 30,
-        'short_url_length_https': 10}
-    assert twitter.url_length == 30
-    twitter._make_client.assert_called()
-
-    mock_client.configuration.return_value = {
-        'short_url_length': 20,
-        'short_url_length_https': 40}
-    assert twitter.url_length == 40
 
 
 def test_update_status(twitter):
